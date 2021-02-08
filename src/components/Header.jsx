c70f910b3281de4ddd96150b096e7a534b6dbdb0
vadym_boyarchuk <vadymboyarchuk@gmail.com>
2021-02-08 18:14:07 +0200

    add components

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+const   Header =()=>{
+    return   <header className='header'>
+    <img src="https://i.pinimg.com/originals/33/b8/69/33b869f90619e81763dbf1fccc896d8d.jpg"></img>
+ </header> 
+}
+
+
+export default Header;
