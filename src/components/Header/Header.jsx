2f839712e04bfe1637ab95b204747295d6bd9eb7
vadym_boyarchuk <vadymboyarchuk@gmail.com>
2021-02-09 18:18:39 +0200

    structuring folders , new components

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+
+import s from './Header.module.css';
+
+const   Header =()=>{
+    return   <header className={s.header}>
+    <img  src="https://i.pinimg.com/originals/33/b8/69/33b869f90619e81763dbf1fccc896d8d.jpg"></img>
+ </header> 
+}
+
+
+export default Header;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,24 @@
+import s from './Navbar.module.css';
+
+const Navbar =()=>{
+    return   <nav className={s.nav}>
+    <div className={`${s.item} ${s.active}`}>
+      <a>Profile</a>
+    </div>
+    <div className={`${s.item} ${s.active}`}>
+      <a>Messages</a>
+    </div>
+    <div className={`${s.item} ${s.active}`}>
+      <a>News</a>
+    </div>
+    <div className={`${s.item} ${s.active}`}>
+      <a>Music</a>
+    </div>
+    <div className={`${s.item} ${s.active}`}>
+      <a>Settings</a>
+    </div>
+  </nav>
+}
+
+
+export default Navbar;
