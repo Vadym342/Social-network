4e56a3f75a4c0558b36b6bdbb50adb89f9d4e51d
Vadym342 <vadimca1.boyarchuc@gmail.com>
2021-02-22 15:34:10 +0200

    refactoring state to store

--- src/index.js
+++ src/index.js
@@ -2,24 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
-
-import {addPost, subscribe, updateNewPostText} from './redux/state';
 import { BrowserRouter } from 'react-router-dom';
-import state from './redux/state'
+import store from './redux/state';
+
 
 
  let rerederEntireTree = (state)=>{
 ReactDOM.render(
     <BrowserRouter>
  {/*  <React.StrictMode> */}
-    <App state={state} addPost={addPost} updateNewPostText={updateNewPostText}  />
+    <App state={store.getState()} 
+    addPost={store.addPost.bind(store)} 
+    updateNewPostText={store.updateNewPostText.bind(store)}  />
  {/*  </React.StrictMode>, */}
-  </BrowserRouter>,document.getElementById('root')
+  </BrowserRouter>,document.getElementById('root') 
 );
 
 
 }
-rerederEntireTree(state);
+rerederEntireTree(store.getState());
 
-subscribe(rerederEntireTree);
+store.subscribe(rerederEntireTree);
